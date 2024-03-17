cc4730e8f9ffe37ee06b6a581c465d8a3b382891
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-03-17 15:26:15 +0100

    Closes #4
    Ajout templates Header/Footer

--- controller/music.go
+++ controller/music.go
@@ -1,1 +1,22 @@
 package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"groupietracker/back"
+	InitTemps "groupietracker/temps"
+	"net/http"
+	"strconv"
+)
+
+func Header(w http.ResponseWriter) bool {
+	if back.Jeu.Header.Albums.Href == "" {
+		if back.Body, back.Fail = back.RequestApi("https://api.spotify.com/v1/browse/new-releases?limit=5"); back.Fail.Error.Status != 200 {
+			fmt.Println("-----------------Erreur :-----------------", back.Fail)
+			InitTemps.Temp.ExecuteTemplate(w, strconv.Itoa(back.Fail.Error.Status), back.Jeu)
+			return false
+		}
+		json.Unmarshal(back.Body, &back.Jeu.Header)
+	}
+	return true
+}
